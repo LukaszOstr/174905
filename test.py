b0648acf2964f854f13ff7c7a0a5ac177ae392e9
LukaszOstr <ostrowkalukasz@wp.pl>
2024-04-03 17:01:52 +0200

    lab7

--- test.py
+++ test.py
@@ -1,35 +1,63 @@
+import math
+import random
 import sys
 
-lista = [6, 4, 9.8, 10, 2]
-print(lista)
-lista.sort()
-print(lista)
-lista.append(8)
-print(lista)
-lista.insert(3, 54)
-print(lista)
-del lista[3]
-print(lista)
-lista.reverse()
-print(lista)
+#Zad1
+suma = 0
+for x in range(25,41):
+    suma += math.pow(math.sin(x) + math.log(36,4),1/3)
+suma = round(suma,2)
+#print(suma)
 
-znaki = 'abcde'
-print(znaki[2])
-print(type())
+#Zad2
+def funkcja(min, max, ile, n):
+    if min > max:
+        return "error"
 
-slownik = {'a': 1, 'b': 2, 'c': 3}
-print(slownik)
-print(slownik['a'])
-slownik.pop('b')
-print(slownik)
-print(slownik.keys())
-print(slownik.values())
+    lista = []
+    for i in range(ile):
+        lista.append(random.randint(min,max))
+    k = 0
+    for j in lista:
+        if(k == n):
+            sys.stdout.writelines("\n")
+            k = 0
+        temp = str(j)
+        sys.stdout.writelines(temp)
+        sys.stdout.writelines(" ")
 
-# liczba = input('Wprowadz liczbe: ')
-# print(liczba + ' to wczytana liczba')
-# sys.stdout.write('Wprowadz liczbe: ')
-# liczba = sys.stdin.readline()
-# print(type(liczba))
-# liczba = int(liczba)
-# print(type(liczba))
-# print(liczba + ' to wczytana liczba')
+        k += 1
+
+min = 2
+max = 9
+ile = 25
+n = 5
+#funkcja(min, max,ile,n)
+
+
+#Zad3
+def plik(nazwa_pliku):
+    a = open(nazwa_pliku,"r")
+    b = a.readlines()
+    for line in b:
+        temp = [int(i) for i in line.split()]
+        min = temp[0]
+        for i in temp:
+            if (i < min):
+                min = i
+        print(min)
+
+#plik('liczby.txt')
+
+
+
+#Zad4
+def pole(a,h):
+    if(a<0 or h<0):
+        return "Dlugosci nie moga byc ujemne"
+    else:
+        return a*a*h
+a = int(input("Wpisz dlugosc boku podstawy: "))
+h = int(input("Wpisz wysokosc graniastoslupa: "))
+print(pole(a,h))
+
